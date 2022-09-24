039ee0d2ec4fe91a54f5e952b16be4789e6865aa
TAMANAM PRABHUDEV <94839973+Prabhudevhimalayan@users.noreply.github.com>
2022-09-24 09:59:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ChangeColor from "./component/ChangeColor";
+function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <ChangeColor />
+    </div>
+  );
+}
+export default App;
